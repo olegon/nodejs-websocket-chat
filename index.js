9a4fcbcdaa107af1998dd2df9ea64d746d3ab07e
Leandro Oliveira <olegon.main@gmail.com>
2016-07-21 22:38:48 -0300

    - Criação do "Server BOT"; - Melhorias no CSS; - Adicionado scroll customizado em -webkit; - Removido endereço fixo para `listen`;

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(http);
 const getName = (() => {
     let counter = 0;
 
-    return () => `User #${counter++}`;
+    return () => `User#${counter++}`;
 })();
 
 const getRandomNumber = () => Math.ceil(127 + (Math.random() * 100) % 64);
@@ -25,29 +25,42 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
     let username = getName();
     const color = getRandomColor();
-    const newNickRegex = /^\\n\s(\w{1,16})/i;
-
-    console.log('a user connected');
+    const newNickRegex = /^\\n\s([a-záàâãéèêíïóôõöúçñ_]{1,32})$/i;
 
     socket.on('disconnect', function() {
-        console.log('user disconnected');
+
     });
 
     socket.on('chat message', function(message) {
         const match = newNickRegex.exec(message);
+
         if (match) {
             const newUsername = match[1];
+
+            io.emit('chat message', {
+                username: 'Server BOT',
+                color: '#c00',
+                message: `O usuário "${username}" trocou seu nome para "${newUsername}"`
+            });
+
             username = newUsername;
         }
+        else {
+            io.emit('chat message', {
+                username: username,
+                color: color,
+                message: message
+            });
+        }
+    });
 
-        io.emit('chat message', {
-            username: username,
-            color: color,
-            message: message
-        });
+    io.emit('chat message', {
+        username: '# Server BOT',
+        color: '#c00',
+        message: `O usuário ${username} entrou na sala.`
     });
 });
 
-http.listen(3000, '192.168.10.125', function() {
-    console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('Escutando na porta 3000');
 });
