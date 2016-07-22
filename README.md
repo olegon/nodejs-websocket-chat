aa58220f1ddefd234a84166678b24156c6bfb443
leandro <olegon.main@gmail.com>
2016-07-22 15:55:38 -0300

    - Melhorado o README.md

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Chat escrito com Node.js, Socket.io, Webpack, Babel-ES6 e Flexbox. :)
 
 ## Como iniciar o servidor?
 
-Execute:
+1. Execute os comandos abaixos para instalar, construir e iniciar o servidor.
 
 ```bash
 $ npm install
 $ npm run build
 $ npm start
 ```
+
+2. Acesse `http:\\localhost:3000` de um navegador.
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require('./server');
 
 const PORT = 3000;
 
-server.listen(PORT, '192.168.10.125', function () {
+// server.listen(PORT, '192.168.10.125', function () {
+//     console.log(`Escutando na porta ${PORT}.`);
+// });
+
+server.listen(PORT, function () {
     console.log(`Escutando na porta ${PORT}.`);
 });
