114350e973fa1f5abbbf8da807db28506eecb7bd
leandro <olegon.main@gmail.com>
2016-07-22 12:59:09 -0300

    - Reorganização dos arquivos.

--- index.js
+++ index.js
@@ -1,65 +1,7 @@
-"use strict";
+const server = require('./server');
 
-const path = require('path');
+const PORT = 3000;
 
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-const usuario = require('./usuario');
-
-app.use('/static', express.static(path.join(__dirname, '/client/dist')));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
-});
-
-const serverBot = usuario.novo('Server BOT', '#c00');
-
-io.on('connection', function(socket) {
-    const user = usuario.novo();
-
-    const newNickRegex = /^\\n\s([a-záàâãéèêíïóôõöúçñ_]{1,32})$/i;
-
-    socket.on('disconnect', function() {
-        broadcastMessage(serverBot, `O usuário ${user.username} saiu da sala.`);
-    });
-
-    socket.on('new-message', function(message) {
-        const match = newNickRegex.exec(message);
-
-        if (match) {
-            const newUsername = match[1];
-
-            broadcastMessage(serverBot, `O usuário "${user.username}" trocou seu nome para "${newUsername}"`);
-
-            user.username = newUsername;
-        } else {
-            broadcastMessage(user, message);
-        }
-    });
-
-    sendMessage(socket, serverBot, `Esse chat foi escrito com Node.js, Socket.io, Webpack e Babel-ES6.`);
-
-    broadcastMessage(serverBot, `O usuário ${user.username} entrou na sala.`);
-
-
-    function broadcastMessage(usuario, mensagem) {
-        sendMessage(io, usuario, mensagem);
-    }
-
-    function sendMessage(socket, usuario, mensagem) {
-        const message = {
-            username: usuario.username,
-            color: usuario.color,
-            message: mensagem
-        };
-
-        socket.emit('new-message', message);
-    }
-});
-
-http.listen(3000, function() {
-    console.log('Escutando na porta 3000');
+server.listen(PORT, function () {
+    console.log(`Escutando na porta ${PORT}.`);
 });
---
+++ server/index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const path = require('path');
+
+const express = require('express');
+const expressApp = express();
+const httpServer = require('http').Server(expressApp);
+const io = require('socket.io')(httpServer);
+
+const usuario = require('./usuario');
+
+expressApp.use('/static', express.static(path.join(__dirname, '../client/dist')));
+
+expressApp.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const serverBot = usuario.novo('* Server BOT *', '#c00');
+
+io.on('connection', function(socket) {
+    const user = usuario.novo();
+
+    const newNickRegex = /^\\n\s([a-záàâãéèêíïóôõöúçñ_]{1,32})$/i;
+
+    socket.on('disconnect', function() {
+        broadcastMessage(serverBot, `O usuário ${user.username} saiu da sala.`);
+    });
+
+    socket.on('new-message', function(message) {
+        const match = newNickRegex.exec(message);
+
+        if (match) {
+            const newUsername = match[1];
+
+            broadcastMessage(serverBot, `O usuário "${user.username}" trocou seu nome para "${newUsername}"`);
+
+            user.username = newUsername;
+        } else {
+            broadcastMessage(user, message);
+        }
+    });
+
+    sendMessage(socket, serverBot, `Esse chat foi escrito com Node.js, Socket.io, Webpack e Babel-ES6. :)`);
+
+    broadcastMessage(serverBot, `O usuário ${user.username} entrou na sala.`);
+
+
+    function broadcastMessage(usuario, mensagem) {
+        sendMessage(io, usuario, mensagem);
+    }
+
+    function sendMessage(socket, usuario, mensagem) {
+        const message = {
+            username: usuario.username,
+            color: usuario.color,
+            message: mensagem
+        };
+
+        socket.emit('new-message', message);
+    }
+});
+
+module.exports = httpServer;
