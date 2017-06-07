ef1164054a7979626c9a0deaf07ea07a0140fa98
olegon <olegon.main@gmail.com>
2017-06-07 14:38:00 -0300

    Porta HTTP é obtida via ENV VAR

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const server = require('./server');
 
-const PORT = 8000;
-
 // server.listen(PORT, '192.168.1.2', function () {
 //     console.log(`Escutando na porta ${PORT}.`);
 // });
 
-server.listen(PORT, function () {
-    console.log(`Escutando na porta ${PORT}.`);
+server.listen(process.env.PORT, function () {
+    console.log(`Escutando na porta ${process.env.PORT}.`);
 });
