6c8e0ef07a10f3c2d49a0780624b1c15c0098f02
olegon <olegon.main@gmail.com>
2017-07-13 09:49:51 -0300

    - Pequena correção (var -> const)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
     entry: {
